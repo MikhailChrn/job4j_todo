75cc083c161989174c0e5e80f2a4b8122336bef2
MikhailChrn <mikhail.cherneyev@yandex.ru>
2025-02-13 17:39:35 +0400

    Add IndexController with html-template

---
+++ src/test/java/ru/job4j/controller/IndexControllerTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class IndexControllerTest {
+
+    @Test
+    public void whenRequestIndexPageThenGetIndexPage() {
+
+        IndexController indexController = new IndexController();
+
+        String view = indexController.getIndex();
+
+        assertThat(view).isEqualTo("index");
+    }
+
+}
